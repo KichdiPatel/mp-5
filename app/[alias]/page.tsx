5ed4f7f8ceaa9fc61562de304e0134a79077e668
KichdiPatel <ayanpatel09@gmail.com>
2024-11-22 20:55:11 -0500

    attempt 3 to fix deployment issue

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -3,12 +3,12 @@
 import { redirect } from "next/navigation";
 import getSiteByAlias from "@/lib/getSiteByAlias";
 
-export default async function AliasPage({
+export default async function FullPostPage({
   params,
 }: {
-  params: { alias: string };
+  params: Promise<{ alias: string }>;
 }) {
-  const { alias } = params;
+  const { alias } = await params;
 
   const site = await getSiteByAlias(alias);
 
